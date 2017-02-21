6b8ccf1fc146324e4ec4c9eb272252498a646d2f
DannyPuckett <danny.puckett1@marist.edu>
2017-02-21 04:59:53 -0500

    lab 3 and lab 4

---
+++ fourth lab.sql
@@ -0,0 +1,74 @@
+﻿/* Danny Puckett Lab 4 CMPT 308 - Database Management 02/20/17 */
+//Question 1.
+select city
+from agents
+where aid in (select aid
+             from orders
+             where cid = 'c006')
+;
+//Question 2.
+select pid, name 
+from products
+where pid in (select distinct pid
+              from orders
+              where aid in(select aid
+                           from orders
+                           where cid in (select cid
+                                         from customers
+                                         where city = 'Kyoto')))
+order by pid DESC
+;
+
+//Question 3.
+select cid, name
+from customers
+where cid in (select distinct cid
+              from orders
+              where cid not in (select distinct cid
+                                from orders
+                                where aid = 'a01'))
+;
+//Question 4.
+select name, cid
+from customers
+where cid in (select distinct cid
+              from orders
+              where pid = 'p07' and cid in (select distinct cid
+                                            from orders
+                                            where pid = 'p01'))
+;
+//Question 5.
+select name, pid
+from products
+where pid in (select distinct cid
+              from orders
+              where aid = 'a08')
+order by pid DESC
+;
+//Question 6.
+select name, discount, city
+from customers
+where cid in (select distinct cid
+              from orders
+              where aid in (select aid
+                            from agents
+                            where city = 'Tokyo'
+                               or city = 'New York'))
+;
+//Question 7.
+select name, cid
+from customers
+where discount in (select discount
+                   from customers
+                   where city = 'London'
+                      or city = 'Duluth' )
+;
+
+/* if we are actually asking for simlar discounts between **AGENTS** in Duluth or London mixed up discount for commissionPct...
+select name, aid
+from agents
+where commissionPct in (select commissionPct
+                        from agents
+                        where city = 'London'
+                           or city = 'Duluth' )
+;*/
---
+++ third lab.sql
@@ -0,0 +1,55 @@
+﻿/*Danny Puckett Lab 3 Database Management CMPT 308   02/20/17  */
+
+//Question 1.
+select ordnumber, totalusd
+from orders
+;
+//Question 2.
+select distinct name, city
+from agents
+where name = 'Smith'
+;
+//Question 3.
+select distinct pid, name, priceUSD
+from products
+where quantity > 200100
+;
+//Question 4.
+select distinct name, city
+from customers
+where city = 'Duluth'
+;
+//Question 5.
+select distinct name /*, city   [we have two agents named smith, could use a city column to decifer the two apart!]*/
+from agents
+where city <> 'New York' 
+   or city <> 'Duluth'
+;
+
+//Question 6.
+select *
+from products
+where priceUSD >= 1.00 
+  and city in ( select city
+               from products
+               where city <> 'Dallas'
+               and city <> 'Duluth'
+             ) 
+;
+//Question 7.
+select *
+from orders
+where month = 'Feb'
+  or month = 'May'
+;
+//Question 8.
+select *
+from orders
+where totalUSD >= 600.00
+  and month = 'Feb'
+;
+//Question 9.
+select *
+from orders
+where cid = 'c005'
+;

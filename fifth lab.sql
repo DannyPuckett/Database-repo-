021c143f9d0e56f634962d5f3b51392295e79f43
DannyPuckett <danny.puckett1@marist.edu>
2017-05-03 00:57:29 -0400

    Add files via upload

--- fifth lab.sql
+++ fifth lab.sql
@@ -1,4 +1,4 @@
-﻿--Danny Puckett	   Lab 5    Database Management Systems
+﻿--Danny Puckett	   Lab 5    CMPT 308 Database Management Systems
 --Question 1.
 select agents.city
 from agents inner join orders    on agents.aid = orders.aid
@@ -8,10 +8,10 @@ order by agents.city ASC;
 
 
 --Question 2  -- problem on this question
-select orders.pid
-from orders inner join agents on orders.aid = agents.aid
-            inner join customers on orders.aid = customers.cid
-where orders.cid='c006'
+select distinct custOrder.pid 
+from orders ord inner join customers on ord.cid = customers.cid
+                inner join agents on customers.city = 'Kyoto'
+                inner join orders custOrder on custOrder.aid = ord.aid
 order by pid DESC;
 
 --Question 3.
@@ -19,8 +19,7 @@ select *
 from customers
 where cid not in (select distinct customers.cid
                   from customers inner join orders on customers.cid = orders.cid
-                  order by cid ASC
-)
+                  order by cid ASC)
 ;
 
 --Question 4.
@@ -28,4 +27,31 @@ select customers.cid
 from customers left outer join orders on customers.cid = orders.cid
 where orders.cid is null
 ;
+
+--Question 5.
+select distinct cust.name, agent.name
+from customers cust inner join orders ord on ord.cid = cust.cid
+                    inner join agents Agent on ord.aid = agent.aid
+where cust.city = agent.city
+;
+
+--Question 6.
+select customers.name, agents.name, customers.city
+from customers full outer join agents on customers.city = agents.city
+where customers.city = agents.city
+;
+
+--Question 7.
+select customers.name, customers.city
+from products inner join customers on products.city = customers.city
+              inner join agents on customers.city = products.city
+where products.city = 'Duluth'
+group by customers.name, customers.city
+;
+
+
+
+
+
+
                   
---
+++ sixth lab.sql
@@ -0,0 +1,95 @@
+﻿--Danny Puckett	   Lab 6    CMPT 308 Database Management Systems
+--Question 1.
+select cust.name, most.city
+from customers cust,( select products.city
+                      from products
+                      group by city
+                      having count(city) =( select count(city)
+                                            from products
+                                            group by city
+                                            order by count(city) desc limit 1
+                                           ) 
+                     ) as most
+where cust.city = most.city
+;
+
+--Question 2.
+select name
+from products
+where priceUSD >( select avg(priceUSD)
+                  from products
+                 )
+order by name desc
+;
+
+--Question 3.
+select customers.name, ord.pid, sum(totalUSD)
+from orders ord inner join customers on ord.cid = customers.cid
+group by customers.name, ord.pid
+order by sum(totalUSD) asc
+;
+
+--Question 4.
+select coalesce(name), coalesce (sum(totalUSD))
+from orders inner join customers on orders.cid = customers.cid
+group by name
+order by name asc
+;
+
+--Question 5.
+select customers.name customer, products.name product, agents.name agent
+from customers inner join orders on customers.cid = orders.cid
+               inner join products on orders.pid = products.pid
+               inner join agents on orders.aid = agents.aid
+where agents.city = 'Newark'
+;
+
+--Question 6.
+select orders.totalUSD, (orders.qty * products.priceUSD) * (1 - (customers.discount/100)) as "Calculated_orderTotal"
+from orders inner join products on orders.pid = products.pid
+            inner join customers on orders.cid = customers.cid
+where orders.totalUSD != ((orders.qty * products.priceUSD) * (1 - (customers.discount/100)))
+;
+
+--Question 7.
+select customers.city, orders.totalUSD
+from customers left outer join orders on customers.cid = orders.cid 
+;
+
+select customers.city, orders.totalUSD
+from customers right outer join orders on customers.cid = orders.cid 
+;
+
+
+-- The example above is an attempt to show the difference between a left outer and a right outer join condition.
+-- Yet it won't suffice, do to being uniformly similar data sets.
+-- Although we were provided an example in class that absolutely would suffice!
+-- Here is that sql code...
+
+CREATE TABLE english (
+	num TEXT NOT NULL,
+	val INT NOT NULL
+);
+
+INSERT INTO english(num, val) VALUES ('one', 1);
+INSERT INTO english(num, val) VALUES ('tow', 2);
+INSERT INTO english(num, val) VALUES ('three', 3);
+
+CREATE TABLE chinese (
+	num TEXT NOT NULL,
+	val INT NOT NULL
+);
+
+INSERT INTO chinese(num, val) VALUES ('ar', 2);
+INSERT INTO chinese(num, val) VALUES ('sun', 3);
+INSERT INTO chinese(num, val) VALUES ('si', 4);
+
+SELECT *
+FROM english LEFT OUTER JOIN chinese ON english.val = chinese.val;
+
+SELECT *
+FROM chinese RIGHT OUTER JOIN english ON english.val = chinese.val;
+
+
+
+
